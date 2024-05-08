80c9c5f1345ac1e43433a38326906f9d3661ae6a
j.szczublinska <joasia.gsu@op.pl>
2024-05-09 01:19:18 +0200

    zmienilam nazwy na nasze w wiekszosci

---
+++ src/main/java/pl/put/poznan/transformer/app/BuildingInfoApplication.java
@@ -0,0 +1,38 @@
+package pl.put.poznan.transformer.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.put.poznan.transformer.dataStructures.*;
+
+
+@SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
+public class BuildingInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildingInfoApplication.class, args);
+
+        System.out.printf("hello \n");
+
+        // making a buidling
+        CompositeLocation mainBuilding = new CompositeLocation(1, "Centrum wykladowe");
+
+        // making levels
+        CompositeLocation level0 = new CompositeLocation(101, "parter");
+        CompositeLocation level1 = new CompositeLocation(102, "1 pietro");
+        mainBuilding.addLocation(level0);
+        mainBuilding.addLocation(level1);
+
+        // making rooms
+        Room room1 = new Room(201, "pokoj Lesia", 10, 2, 2, 50, 20);
+        Room room2 = new Room(202, "pokoj Dareczka", 60, 120, 2.5, 85, 24.9);
+
+        // adding them the the level0
+        level1.addLocation(room1);
+        level1.addLocation(room2);
+
+        System.out.printf(String.format("%s pokoj ma  powierzchnie %f m^2 \n and volume %f m^3", room1.getName(), room1.calculateTotalArea(), room1.calculateTotalVolume()));;
+
+    }
+
+
+}
---
+++ src/main/java/pl/put/poznan/transformer/logic/BuildingInfo.java
@@ -0,0 +1,10 @@
+package pl.put.poznan.transformer.logic;
+
+/**
+ * This is just an example to show that the logic should be outside the REST service.
+ */
+public class BuildingInfo {
+
+    // to tutaj bedziemy dodawac obiekty moim zdaniem, w czwartek to dokoncze
+
+}
