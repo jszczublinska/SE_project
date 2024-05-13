0be43c6a423769a2aa999e54d3d3571c93236d89
Wikusia-s <wik.szarzynska@gmail.com>
2024-05-13 10:57:19 +0200

    volume function

--- src/main/java/pl/put/poznan/info/logic/dataStructures/CompositeBuilding.java
+++ src/main/java/pl/put/poznan/info/logic/dataStructures/CompositeBuilding.java
@@ -93,7 +93,11 @@ public class CompositeBuilding implements ComponentLocation {
     @Override
     public double calculateTotalVolume() {
         // TO IMPLEMENT WIKTORIA
-        return 0.0;
+        double totalVolume = 0.0;
+        for(ComponentLocation location: listOfLevels){
+            totalVolume += location.calculateTotalVolume();
+        }
+        return totalVolume;
     }
 
     /**
