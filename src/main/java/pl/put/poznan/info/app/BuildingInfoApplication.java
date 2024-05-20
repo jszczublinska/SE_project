b95ba53122a0c8a00824dd88b0f49dedd91af350
Bialkasss <bialkass@gmail.com>
2024-05-20 05:52:28 +0200

    changes

--- src/main/java/pl/put/poznan/info/app/BuildingInfoApplication.java
+++ src/main/java/pl/put/poznan/info/app/BuildingInfoApplication.java
@@ -1,5 +1,7 @@
 package pl.put.poznan.info.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -16,11 +18,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.info.rest"})
 public class BuildingInfoApplication {
 
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(BuildingInfoApplication.class);
+
     /**
      *
      * @param args The arguments provided while running the program
      */
     public static void main(String[] args) {
+        logger.info("Starting application");
         SpringApplication.run(BuildingInfoApplication.class, args);
         //http://localhost:8080/
 
