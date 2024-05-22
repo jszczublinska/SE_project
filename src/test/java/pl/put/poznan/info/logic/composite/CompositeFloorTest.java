45526f7cab43c5d4d7702e1092db82ea797189a9
Bialkasss <bialkass@gmail.com>
2024-05-22 17:03:30 +0200

    adding composite floor test thats should work

--- src/test/java/pl/put/poznan/info/logic/composite/CompositeFloorTest.java
+++ src/test/java/pl/put/poznan/info/logic/composite/CompositeFloorTest.java
@@ -1,41 +1,54 @@
-//package pl.put.poznan.info.logic.composite;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.ArrayList;
-//
-//import static org.mockito.Mockito.*;
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.Mockito.*;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class CompositeFloorTest {
-//
-//    @Test
-//    void calculateTotalArea() {
-//        Room mockRoom = mock(FancyDatabase.class);
-//        when(DbMock.queryAll()).thenReturn(new ArrayList<>());
+package pl.put.poznan.info.logic.composite;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompositeFloorTest {
+
+    //    @BeforeEach
+//    public void setUp(){
+//        List<ComponentLocation> listOfRooms = Arrays.asList(
+//                new Room(),
+//                new Room(),
+//                new Room()
+//        );
 //    }
-//
-//    @Test
-//    void calculateTotalVolume() {
-//    }
-//
-//    @Test
-//    void calculateTotalHeatingEnergy() {
-//    }
-//
-//    @Test
-//    void calculateTotalLightingPower() {
-//    }
+    @Test
+    void calculateTotalAreaTest() {
+        ArrayList<ComponentLocation> listOfRooms = new ArrayList<ComponentLocation>(Arrays.asList(
+                new Room("112", 13, 200, 5, 9),
+                new Room("111", 20, 400, 15, 30),
+                new Room("110", 80, 600, 21, 60)
+        ));
 
+        CompositeFloor mockFloor = mock(CompositeFloor.class);
+        when(mockFloor.getList()).thenReturn(listOfRooms);
 
+        assertEquals(113.0, mockFloor.calculateTotalArea());
+    }
 
+    @Test
+    void calculateTotalVolumeTest() {
+    }
 
+    @Test
+    void calculateTotalHeatingEnergyTest() {
+    }
 
+    @Test
+    void calculateTotalLightingPowerTest() {
+    }
 
-//
-//    @Test
-//    void calculateTotalWaterConsumption() {
-//    }
-//}
+    @Test
+    void calculateTotalWaterConsumptionTest() {
+    }
+}
