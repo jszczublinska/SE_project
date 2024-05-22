ceba73d9a59ff94849d258a26a0d7126c1f5fc64
Bialkasss <bialkass@gmail.com>
2024-05-22 15:28:55 +0200

    added 5 tests basic

--- src/test/java/pl/put/poznan/info/logic/composite/RoomTest.java
+++ src/test/java/pl/put/poznan/info/logic/composite/RoomTest.java
@@ -1,28 +1,41 @@
 package pl.put.poznan.info.logic.composite;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoomTest {
 
+    private Room room;
+
+    @BeforeEach
+    public void steUp(){
+        room = new Room("112", 13, 200, 5, 9);
+    }
+
     @Test
     void calculateTotalArea() {
+        assertEquals(13.0, room.calculateTotalArea() );
     }
 
     @Test
     void calculateTotalVolume() {
+        assertEquals(200.0, room.calculateTotalVolume());
     }
 
     @Test
     void calculateTotalHeatingEnergy() {
+        assertEquals(9, room.calculateTotalHeatingEnergy());
     }
 
     @Test
     void calculateTotalLightingPower() {
+        assertEquals(5, room.calculateTotalLightingPower());
     }
 
     @Test
     void calculateTotalWaterConsumption() {
+        assertEquals(2*13, room.calculateTotalArea());
     }
 }
