f08f97fc071d789c30e99ceff4589c515d9cc09d
Bialkasss <bialkass@gmail.com>
2024-05-22 15:17:31 +0200

    creating test class

---
+++ src/test/java/pl/put/poznan/info/logic/composite/RoomTest.java
@@ -0,0 +1,28 @@
+package pl.put.poznan.info.logic.composite;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+    @Test
+    void calculateTotalArea() {
+    }
+
+    @Test
+    void calculateTotalVolume() {
+    }
+
+    @Test
+    void calculateTotalHeatingEnergy() {
+    }
+
+    @Test
+    void calculateTotalLightingPower() {
+    }
+
+    @Test
+    void calculateTotalWaterConsumption() {
+    }
+}
