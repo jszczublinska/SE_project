d9272ef8ab6ccf2407f9c8da406a810e495811b3
Bialkasss <bialkass@gmail.com>
2024-05-22 15:48:43 +0200

    mock

---
+++ src/test/java/pl/put/poznan/info/logic/composite/CompositeFloorTest.java
@@ -0,0 +1,35 @@
+//package pl.put.poznan.info.logic.composite;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.ArrayList;
+//
+//import static org.mockito.Mockito.*;
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.mockito.Mockito.*;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class CompositeFloorTest {
+//
+//    @Test
+//    void calculateTotalArea() {
+//        Room mockRoom = mock(FancyDatabase.class);
+//        when(DbMock.queryAll()).thenReturn(new ArrayList<>());
+//    }
+//
+//    @Test
+//    void calculateTotalVolume() {
+//    }
+//
+//    @Test
+//    void calculateTotalHeatingEnergy() {
+//    }
+//
+//    @Test
+//    void calculateTotalLightingPower() {
+//    }
+//
+//    @Test
+//    void calculateTotalWaterConsumption() {
+//    }
+//}
